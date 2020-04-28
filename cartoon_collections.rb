80d6275439cef00afcbc05fbbde7884c0bac0d7e
Kosi Akporji <kosiazom@gmail.com>
2020-04-28 22:57:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,14 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+
+  array.each do |array|
+    puts "Hello #{array.capitalize}!"
+  end
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  i  = 0 
+  array.each_with_index do |dwarf, index += 1 |
+    puts "#{1}. *#{dwarf.capitalize}"
+  end
+  
 end
